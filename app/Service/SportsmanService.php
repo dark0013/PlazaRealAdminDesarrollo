496db1b4f50430fb86ebbb473b2a470205258b64
dark0013 <andresarcangel00@gmail.com>
2026-01-27 01:15:27 -0500

    se agrego la descripción categoria en el deportista

--- app/Service/SportsmanService.php
+++ app/Service/SportsmanService.php
@@ -3,14 +3,35 @@ namespace App\Service;
 
 use App\Models\Sportsman;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class SportsmanService
 {
-    public function getAllSportsman()
-    {
-        $sportsmen = Sportsman::all();
-        return $sportsmen->isEmpty() ? null : $sportsmen;
-    }
+  public function getAllSportsman()
+{
+    $sportsmen = DB::table('sportsman as s')
+        ->leftJoin('category as c', 'c.id', '=', 's.category')
+        ->select(
+            's.id',
+            's.name',
+            's.surname',
+            's.identification',
+            's.birthdate',
+            's.gender',
+            's.telephone',
+            's.email',
+            's.current_ranking',
+            's.status',
+
+            // datos de categoría
+            'c.id as category_id',
+            'c.name as category_name',
+            'c.description as category_description'
+        )
+        ->get();
+
+    return $sportsmen->isEmpty() ? null : $sportsmen;
+}
 
     public function getSportsmanById(int $id)
     {
